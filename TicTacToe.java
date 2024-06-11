2a8489d40f260c8e70aa0d1dad888a0c07d44452
Micah Leifer <98467381+miclei25@users.noreply.github.com>
2024-06-11 13:58:43 -0400

    button

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,9 +1,16 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class TicTacToe extends JFrame {
+    int ROWS = 9;
+    int COLUMNS = 9;
+
     public TicTacToe() {
         setTitle("Tic-Tac-Toe");
-        setSize(750, 750);
+        setSize(810, 810);
+        setLayout(new GridLayout(ROWS, COLUMNS));
+        
+        new JButton("hi");
 
         setVisible(true);
     }
