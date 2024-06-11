3c27285e98af27cdfeff77699d0970bb8fd49484
Micah Leifer <98467381+miclei25@users.noreply.github.com>
2024-06-11 13:53:15 -0400

    created a window

---
+++ TicTacToe.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class TicTacToe extends JFrame {
+    public TicTacToe() {
+        setTitle("Tic-Tac-Toe");
+        setSize(750, 750);
+
+        setVisible(true);
+    }
+}
